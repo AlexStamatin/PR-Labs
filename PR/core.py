cced57f4da776601f47b27b3af9a5ea75aedcace
Myself <xtreamsandu@gmail.com>
2018-03-04 19:33:14 +0200

    Add concurrent requests

--- PR/core.py
+++ PR/core.py
@@ -1,11 +1,30 @@
 
-def reverse(string):
-    string = string[::-1]
-    return string
+import asyncio
+import requests
 
-def square(num):
-    try:
-        float(num)
-        return num**2
-    except ValueError:
-        return False
+headers = {'Accept': 'text/csv',
+           'X-API-Key': '55193451-1409-4729-9cd4-7c65d63b8e76'}
+
+category = 'https://evil-legacy-service.herokuapp.com/api/v101/categories/'
+
+order = 'https://evil-legacy-service.herokuapp.com/api/v101/orders/'
+
+data = {'start': '2018-01-01', 'end': '2018-03-03'}
+
+
+async def getData():
+    def categ():
+        return requests.get('https://evil-legacy-service.herokuapp.com/api/v101/categories/', headers=headers)
+    def ord():
+        return requests.get(order,headers=headers, params=data)
+    loop = asyncio.get_event_loop()
+    future1 = loop.run_in_executor(None, categ)
+    future2 = loop.run_in_executor(None, ord)
+    response1 = await future1
+    response2 = await future2
+    print(response1.text)
+    print(response2.text)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(getData())
+
