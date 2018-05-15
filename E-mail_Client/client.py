2f1c127aa41d3fe482c5afc084b1c64c2dd524b2
Myself <xtreamsandu@gmail.com>
2018-05-15 20:05:08 +0300

    Add Search through emails option

--- E-mail_Client/client.py
+++ E-mail_Client/client.py
@@ -5,17 +5,30 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import getpass
 import os
+
+#gmail SSL SMTP Server
 
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465 # ssl port 465, tls port 587
 
+#gmail SSL IMAP Server
+
 IMAP_SERVER =  'imap.gmail.com'
 IMAP_PORT = 993
 
+#smtp and imap sessions
+
 imap_client = 0
 smtp_client = 0
+
+#user nickname and password
+
 login_nick = ""
+login_pass = ""
+
+#create connection with imap and smtp servers
 
 def login(user, password):
 
@@ -33,26 +46,45 @@ def login(user, password):
     return smtp_ssl,imap_ssl
 
 
+
+# returns number of messages and an array of messages from the mail inbox
+# receives imap session as parameter
 def read(imap):
-    # Login to INBOX
-    # imap = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-    # imap.login(username, password)
+
+
     imap.select('INBOX')
 
     status, response = imap.search(None, 'ALL')
     unread_msg_nums = response[0].split()
 
-    # Print all unread messages from a certain sender of interest
-    status, response = imap.search(None, '(ALL)')
     unread_msg_nums = response[0].split()
-    da = []
+    nr = []
     for e_id in unread_msg_nums:
         _, response = imap.fetch(e_id, '(RFC822)')
-        da.append(email.message_from_bytes(response[0][1]))
+        nr.append(email.message_from_bytes(response[0][1]))
 
-    return da,len(unread_msg_nums)
+    return nr,len(unread_msg_nums)
 
 
+#returns messages which contaned searched string in either sender or subject
+def search(imap,string):
+
+    imap.select('INBOX')
+
+    status, response = imap.search(None, 'ALL')
+    unread_msg_nums = response[0].split()
+
+    # sort messages
+    status, response = imap.search(None, 'ALL', '(OR (FROM "%s") (SUBJECT "%s"))' % (string,string))
+    unread_msg_nums = response[0].split()
+    nr = []
+    for e_id in unread_msg_nums:
+        _, response = imap.fetch(e_id, '(RFC822)')
+        nr.append(email.message_from_bytes(response[0][1]))
+
+    return nr,len(unread_msg_nums)
+
+#returns number of attachments of an email
 def attachments(mail):
     nr = 0
     if mail.get_content_maintype() != 'multipart':
@@ -62,7 +94,7 @@ def attachments(mail):
             nr += 1
     return nr
 
-
+#reutns preview of the text content of the message
 def preview(mail):
     body = ""
     if mail.is_multipart():
@@ -84,6 +116,7 @@ def preview(mail):
 
     return body
 
+#returns full text body of a message
 def readmes(mail):
 
     body = ""
@@ -105,15 +138,27 @@ def readmes(mail):
 
     return body
 
+#checks if connection is still active
+def test_conn_open(conn):
+    try:
+        status = conn.noop()[0]
+    except:  # smtplib.SMTPServerDisconnected
+        status = -1
+    return True if status == 250 else False
 
-def sendMail(server,nick, to, subject, text, files=[]):
+#sends an email with optional attachments and cc fields
+def sendMail(server,nick, to, subject, text, files=[],cc=None):
 
     assert type(files)==list
 
     msg = MIMEMultipart()
     msg['From'] = nick
-    msg['To'] = to
     msg['Subject'] = subject
+    msg['To'] = to
+    if cc:
+        msg['Cc'] = cc
+        to.append(cc)
+    to = [to]
 
     msg.attach(MIMEText(text))
 
@@ -129,30 +174,41 @@ def sendMail(server,nick, to, subject, text, files=[]):
 
     print('Done')
 
-    server.quit()
+# def set_password(self, raw_password):
+#     import random
+#     algo = 'sha1'
+#     salt = get_hexdigest(algo, str(random.random()), str(random.random()))[:5]
+#     hsh = get_hexdigest(algo, salt, raw_password)
+#     self.password = '%s$%s$%s' % (algo, salt, hsh)
 
 
 
 
-def print_menu():       ## Your menu design here
+def print_menu():       #Menu options
     print('\n')
     print('Menu:')
     print("1.Log in")
     print("2.Show inbox")
     print("3.Select message to read")
     print("4.Send message")
+    print("5.Search")
+    print("6.Exit")
 
 
 while True:
     print_menu()
     choice=int(input("Enter Option: "))
     if choice == 1:
-        smtp_client,imap_client = login("pyclient27@gmail.com","prstudent14")
+        login_nick = input("Nickname: ")
+        login_pass = getpass.getpass("Password: ")
+        smtp_client,imap_client = login(login_nick,login_pass)
     elif choice == 2:
+        if not test_conn_open(imap_client):
+            login(login_nick,login_pass)
         mes, nrmes = read(imap_client)
 
         for iter in range(nrmes):
-            print(iter,".")
+            print(iter+1,".")
             print('Sender: ', mes[iter]["From"])
             print('Date: ', mes[iter]["Date"])
             print('Subject: ', mes[iter]["Subject"])
@@ -162,17 +218,54 @@ while True:
                 print('Number of attachments: ', nratt)
             print('Preview: ', prev)
     elif choice == 3:
+        if not test_conn_open(imap_client):
+            login(login_nick,login_pass)
         mes, nrmes = read(imap_client)
         if nrmes != 0:
             print(nrmes,' messages avaialable')
             which = int(input('Message to read: '))
             if which > 0 and which <nrmes:
                 body = readmes(mes[which-1])
+                print(which-1, ".")
+                print('Sender: ', mes[which-1]["From"])
+                print('Date: ', mes[which-1]["Date"])
+                print('Subject: ', mes[which-1]["Subject"])
                 print(body)
             else:
                 print('Outside range')
         else:
             print('No messages to read')
+    elif choice == 4:
+        if not test_conn_open(imap_client):
+            login(login_nick,login_pass)
+        recip = input("Recipient: ")
+        subj = input("Subject: ")
+        text = input("Text: ")
+        cc = input("Additional recipients: ").upper()
+        if cc == 'YES':
+            address = input("cc address: ")
+        else:
+            address = None
+        attach = input("Include files?: ").upper()
+        files = []
+        if attach == "YES":
+            filename = input('Attachment name: ')
+            files.append(filename)
+        sendMail(smtp_client,login_nick, recip, subj, text, files, address)
 
-    elif choice == 10:
+    elif choice == 5:
+        key = input("Search string: ")
+        mes,nrmes = search(imap_client,key)
+        if nrmes != 0:
+            for iter in range(nrmes):
+                print(iter,".")
+                print('Sender: ', mes[iter]["From"])
+                print('Date: ', mes[iter]["Date"])
+                print('Subject: ', mes[iter]["Subject"])
+                nratt = attachments(mes[iter])
+                prev = preview(mes[iter])
+        else:
+            print("Nothing found")
+
+    elif choice == 6:
         break
